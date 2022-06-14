18772c7ec4cb0733e987b9cf3b086003c9452c44
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-06-14 17:39:40 +0100

    Controller added Update 1

--- src/router.js
+++ src/router.js
@@ -3,16 +3,6 @@ const jwt = require("jsonwebtoken");
 const { verifyToken, createToken } = require("./controller");
 
 const router = express.Router();
-// const secret = process.env.JWT_SECRET;
-// const mockUser = {
-//   username: "authguy",
-//   password: "mypassword",
-//   profile: {
-//     firstName: "Chris",
-//     lastName: "Wolstenholme",
-//     age: 43,
-//   },
-// };
 
 router.post("/login", createToken);
 
