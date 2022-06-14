8ec179f2829aef710357c1ca6f859adc88802dc9
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-06-14 17:39:01 +0100

    Controller added

---
+++ src/controller.js
@@ -0,0 +1,50 @@
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
+
+const mockUser = {
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
+};
+
+const createToken = async (req, res) => {
+  // Create a new JWT; the payload should be an object containing the mock user's username.
+  // const token = jwt.sign({ username }, "VerySecret");
+  // Don't worry about working with credentials right now.
+  // // -> extract username and password from the request body
+  const { username, password } = req.body;
+  // // -> compare with the user data (mockUser)
+  if (username !== mockUser.username || password !== mockUser.password) {
+    return res
+      .status(400)
+      .json({ error: "Password or Username is incorrect!" });
+  }
+  // // -> if valid create and send back the token
+  const token = jwt.sign({ username }, secret);
+  // // -> otherwise you'll send back the token
+  // Send the JWT back to the client in a JSON response.
+  res.json({ token });
+};
+
+const verifyToken = async (req, res) => {
+  // Get the token from the authorization header of the request.
+  console.log("req.headers:", req.headers);
+  const [bearer, token] = req.headers.authorization.split(" ");
+  console.log(req.headers.authorization);
+  console.log("Token:", token);
+  // Use console logs to inspect the req object to figure out how to find this.
+  // Use the jsonwebtoken library to verify that the token is valid.
+  try {
+    const encodedIntoPayload = jwt.verify(token, secret);
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+  // Respond with the mock user's profile if the token is valid, or a failure message if it isn't.
+  res.json({ Profile: mockUser.profile });
+};
+
+module.exports = { verifyToken, createToken };
